72a57d9738b2fda6b3f1120c97a146a6bef7881e
Roshan-Sen <97144941+Roshan-Sen@users.noreply.github.com>
2023-11-26 12:21:28 -0800

    Initial Commit 11/26

---
+++ HelloWorld/README.txt
@@ -0,0 +1,5 @@
+Roshan Sen
+11/26/2023
+
+This is a simple program to test printing "Hello World!". To run this program,
+simply compile and run the HelloWorld.java file.
---
+++ HelloWorld/src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * This is a program that prints "Hello World"
+ * Written by Roshan Sen on 11/26/2023
+ **/
+
+public class HelloWorld {
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+	}
+}
